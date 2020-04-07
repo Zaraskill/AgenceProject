b863c2d2dc59c7bcd0ca30fbc9d53fec7203212f
XuX26 <ulysse.raillon@hotmail.fr>
2020-04-07 18:20:46 +0200

    Player Controller / Test Scene / Settings

---
+++ AgenceProject/Assets/Scripts/Ulysse/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float throwForce;
+
+    private Rigidbody2D _rb;
+    private Vector2 _startPosition, _lastPosition, _currentPosition, _direction, _startScale;
+    private bool _throwAllowed = true;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _startPosition = transform.position;
+        _startScale = transform.localScale;
+    }
+
+    void ResetPosition()
+    {
+        transform.position = _startPosition;
+        _rb.velocity = Vector2.zero;
+        _rb.isKinematic = true; //When Stuck
+        _throwAllowed = true; //
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch t = Input.GetTouch(0);
+            _currentPosition = Input.touches[0].position;
+
+            if (t.phase == TouchPhase.Began)
+            {
+                _lastPosition = Input.touches[0].position;
+                transform.localScale = _startScale;
+            }
+            else if (t.phase == TouchPhase.Moved)
+            {
+                _direction = _lastPosition - _currentPosition;
+            }
+            else if (t.phase == TouchPhase.Ended && _throwAllowed)
+            {
+                _direction = _lastPosition - _currentPosition;
+                print(_direction);
+
+                _lastPosition = Vector2.zero;
+                _rb.isKinematic = false;
+                _rb.AddForce(_direction.normalized * throwForce, ForceMode2D.Impulse);
+                _throwAllowed = false;
+                print(_direction.normalized);
+            }
+        }
+    }
+}
