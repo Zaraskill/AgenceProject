5dbf89f730235452bba4f452e30b628a6dfb886c
Flo <dappe.florent@gmail.com>
2020-04-09 11:32:37 +0200

    Flo ~ Add LevelBuilder
    - add Animations
    - add 'LevelData' Folder
    - add 'Object Icons' Folder
    - add 'Recources' Folder
    - add Scene 'LevelBuilder'
    - add Script Folder 'LevelBuilder'

---
+++ AgenceProject/Assets/Scripts/LevelBuilder/CameraMove.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CameraMove : MonoBehaviour
+{
+    public ManagerScript ms;
+
+    private float xAxis;
+    private float yAxis;
+    private float zoom;
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>(); // get the camera component for later use
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ms.saveLoadMenuOpen == false) // if no save or load menus are open.
+        {
+            xAxis = Input.GetAxis("Horizontal"); // get user input
+            yAxis = Input.GetAxis("Vertical");
+
+            //zoom = Input.GetAxis("Mouse ScrollWheel") * 10;
+
+            // move camera based on info from xAxis and yAxis
+            // transform.Translate(new Vector3(xAxis * 1 * -1, 0.0f));
+            
+
+            //change camera's orthographic size to create zooming in and out. Can only be between -25 and -5.
+            //if (zoom < 0 && cam.orthographicSize >= -25)
+            //    cam.orthographicSize -= zoom * -1;
+
+            //if (zoom > 0 && cam.orthographicSize <= -5)
+            //    cam.orthographicSize += zoom * 1;
+        }
+    }
+}
---
+++ AgenceProject/Assets/Scripts/LevelBuilder/EditorObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System;
+
+public class EditorObject : MonoBehaviour
+{
+    public enum ObjectType { Platform_1, Platform_2, Platform_3, Platform_4 };
+
+    [Serializable] // serialize the Data struct
+    public struct Data
+    {
+        public Vector3 pos;
+        public Quaternion rot;
+        public ObjectType objectType;
+    }
+
+    public Data data;
+}
