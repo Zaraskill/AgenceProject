fb477786508d435c2f7d6da54cb4d4ab8f06eb27
Flo <dappe.florent@gmail.com>
2020-04-09 16:26:28 +0200

    Auto stash before rebase of "origin/dev"

---
+++ AgenceProject/Assets/Scripts/Flo/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Menu")]
+    public GameObject PausePanel;
+    
+    void Start()
+    {
+        Time.timeScale = 1f;
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void Pause()
+    {
+        PausePanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        PausePanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
