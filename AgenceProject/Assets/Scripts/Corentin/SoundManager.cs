82b5fe6257373203bfd1af2d1e5487ad2dea0ba5
Zaraskill <corentin.lelong77@gmail.com>
2020-04-10 11:32:51 +0200

    scripts sound/ui manager and ennemy for structure (empty scripts)

---
+++ AgenceProject/Assets/Scripts/Corentin/SoundManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public static SoundManager soundManager;
+
+    //public AudioClip clioType;
+
+    void Awake()
+    {
+        if (soundManager != null)
+        {
+            Debug.LogError("Too many instances!");
+        }
+        else
+        {
+            soundManager = this;
+            DontDestroyOnLoad(soundManager);
+        }
+    }
+
+    private void MakeSound(AudioClip originalClip)
+    {
+        AudioSource.PlayClipAtPoint(originalClip, transform.position);
+    }
+
+    //public void MakeTypeSound()
+    //{
+    //    MakeSound(typeSound);
+    //}
+}
