9c6f776a1380342b55665e90be3262affb086c1b
Flo <dappe.florent@gmail.com>
2020-04-10 10:51:06 +0200

    Flo ~ Level Selector (Work)

--- AgenceProject/Assets/Scripts/Flo/MenuManager.cs
+++ AgenceProject/Assets/Scripts/Flo/MenuManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class MenuManager : MonoBehaviour
     public void selectLevel(int levelId)
     {
         Debug.Log(levelId);
+        GameManager.IntLevel(levelId);
+        SceneManager.LoadScene("Flo_Main");
     }
 
     public void Quit()
