f3d44196038787c2fac1b0e5260a304379b672f5
Flo <dappe.florent@gmail.com>
2020-04-14 15:49:23 +0200

    Flo ~ Kill LevelBuilder (remove)

--- AgenceProject/Assets/Scripts/LevelBuilder/LevelEditor.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-using System;
-
-[Serializable] // serialize the entire class.
-public class LevelEditor
-{
-    public List<EditorObject.Data> editorObjects; // new list of editor object data.
-}
