7070619a70695a9724092f04d21e8f68b176dc90
XuX26 <ulysse.raillon@hotmail.fr>
2020-04-15 12:36:38 +0200

    stickyWall not tested

---
+++ AgenceProject/Assets/Scripts/Ulysse/StickyWall.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyWall : Wall
+{
+    public void ActivateWall(PlayerController player)
+    {
+        player.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+        player.playerState = PlayerState.idle;
+    }
+}
