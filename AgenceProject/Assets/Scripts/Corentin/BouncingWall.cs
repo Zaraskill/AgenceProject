ff4a21cba9dd17f873ba20ed5265938aaaa13037
Zaraskill <corentin.lelong77@gmail.com>
2020-04-17 15:52:25 +0200

    Bouncing all sides

--- AgenceProject/Assets/Scripts/Corentin/BouncingWall.cs
+++ AgenceProject/Assets/Scripts/Corentin/BouncingWall.cs
@@ -4,17 +4,40 @@ using UnityEngine;
 
 public class BouncingWall : MonoBehaviour
 {
-
     private Rigidbody2D _rigidbody;
+    public enum SIDE_JUMP { LEFT, RIGHT, UP, DOWN}
+    //private Vector2 leftUpAngle, rightUpAngle, leftBotAngle, rightBotAngle;
     public float reboundForce = 10f;
-    
+    public SIDE_JUMP sideJump;
+
+    void Awake()
+    {
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();        
+    }
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             _rigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
-            Debug.Log("rb find");
-            _rigidbody.AddForce(transform.up * reboundForce);
+            switch (sideJump)
+            {
+                case SIDE_JUMP.UP:
+                    _rigidbody.AddForce(transform.up * reboundForce);
+                    break;
+                case SIDE_JUMP.RIGHT:
+                    _rigidbody.AddForce(transform.right * reboundForce);
+                    break;
+                case SIDE_JUMP.LEFT:
+                    _rigidbody.AddForce(-transform.right * reboundForce);
+                    break;
+                case SIDE_JUMP.DOWN:
+                    _rigidbody.AddForce(-transform.up * reboundForce);
+                    break;
+                default:
+                    break;
+            }
         }        
-    }
+    }    
 }
--- AgenceProject/Assets/Scripts/Corentin/PlayerEntity.cs
+++ AgenceProject/Assets/Scripts/Corentin/PlayerEntity.cs
@@ -16,16 +16,32 @@ public class PlayerEntity : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _rigidbody.AddForce(new Vector2(50f, 0));
+        //_rigidbody.AddForce(new Vector2(50f, 0));
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Ennemy")
+        if (collision.tag == "Ennemy")
         {
             Destroy(collision.gameObject);
-
+        }
+        else if (collision.tag == "LeftCollider")
+        {
+            collision.GetComponentInParent<BouncingWall>().sideJump = BouncingWall.SIDE_JUMP.LEFT;
+        }
+        else if (collision.tag == "RightCollider")
+        {
+            collision.GetComponentInParent<BouncingWall>().sideJump = BouncingWall.SIDE_JUMP.RIGHT;
+        }
+        else if (collision.tag == "TopCollider")
+        {
+            collision.GetComponentInParent<BouncingWall>().sideJump = BouncingWall.SIDE_JUMP.UP;
+        }
+        else if (collision.tag == "BotCollider")
+        {
+            collision.GetComponentInParent<BouncingWall>().sideJump = BouncingWall.SIDE_JUMP.DOWN;
         }
     }
+
 
 }
