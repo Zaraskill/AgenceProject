f61a723cf16d0b0eb69d28a0d179f9b6c970f764
Flo <dappe.florent@gmail.com>
2020-04-28 11:38:33 +0200

    Correction

--- AgenceProject/Assets/Scripts/Flo/CheckListVelocity.cs
+++ AgenceProject/Assets/Scripts/Flo/CheckListVelocity.cs
@@ -6,20 +6,22 @@ public class CheckListVelocity : MonoBehaviour
 {
     public GameObject PushableComponent;
     public GameObject DestructibleComponent;
+    public Rigidbody2D Player;
 
     public List<Rigidbody2D> Children;
-    public float checkDuration = 1f;
+    //public float checkDuration = 1f;
 
     
     void Start()
     {
+        /*
         foreach (Transform child in transform)
         {
             if (child.tag == "Player" || child.tag == "PushableWall" || child.tag == "DestructibleWall")
             {
                 Children.Add(child.GetComponent<Rigidbody2D>());
             }
-        }
+        }*/
         if (PushableComponent != null)
         {
             foreach (Transform childComponent in PushableComponent.transform)
@@ -47,23 +49,27 @@ public class CheckListVelocity : MonoBehaviour
 
     IEnumerator Checking()
     {
+        yield return new WaitForSeconds(1f);
         bool active = true;
         while (active)
         {
-            int objectDoesntSleep = 0;
-
-            foreach (Rigidbody2D child in Children)
+            if (Player.velocity.magnitude <= 0.01f)
             {
-                if (child != null && child.velocity.magnitude > 0.01f)
-                    objectDoesntSleep++;
+                int objectDoesntSleep = 0;
+
+                foreach (Rigidbody2D child in Children)
+                {
+                    if (child != null && child.velocity.magnitude > 0.01f)
+                        objectDoesntSleep++;
+                }
+
+                if (objectDoesntSleep == 0)
+                {
+                    active = false;
+                }
             }
 
-            if(objectDoesntSleep == 0)
-            {
-                active = false;
-            }
-
-            yield return new WaitForSeconds(checkDuration);
+            yield return new WaitForSeconds(1f);
         }
         Debug.Log("Laucher Ready !");
         PlayerController.throwAllowed = true;
