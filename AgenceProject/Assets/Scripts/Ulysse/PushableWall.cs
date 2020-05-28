bd8bba3eb76dd86330040299874c400f026945ba
Flo <dappe.florent@gmail.com>
2020-05-28 12:58:34 +0200

    MASSIVE UPGRADE GRAPHIC

--- AgenceProject/Assets/Scripts/Ulysse/PushableWall.cs
+++ AgenceProject/Assets/Scripts/Ulysse/PushableWall.cs
@@ -8,18 +8,36 @@ public class PushableWall : MonoBehaviour
     private Vector2 otherVelocity;
     private GameObject parent;
     private Rigidbody2D parentRb;
+    private Material material;
+
+    bool isDissolving;
+    float fade = 2f;
 
     void Awake()
     {
         parent = transform.parent.gameObject;
         parent.AddComponent<PushableDestruction>();
         parentRb = parent.GetComponent<Rigidbody2D>();
+        material = parent.GetComponent<SpriteRenderer>().material;
     }
 
     void FixedUpdate()
     {
         transform.localPosition = Vector3.zero;
         transform.rotation = parent.transform.rotation;
+
+        if (isDissolving)
+        {
+            fade -= Time.deltaTime;
+
+            if (fade <= 0)
+            {
+                fade = 0;
+                isDissolving = false;
+            }
+
+            material.SetFloat("_Fade", fade);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D other)
@@ -29,6 +47,7 @@ public class PushableWall : MonoBehaviour
             otherVelocity = other.GetContact(0).rigidbody.velocity;
             collidepos = other.GetContact(0).point;
             parentRb.AddForceAtPosition(-otherVelocity, collidepos,ForceMode2D.Impulse);
+            isDissolving = true;
         }
     }
 }
