33f7e5088860d40d07ae3e3a63197cf2bdd140bd
XuX26 <ulysse.raillon@hotmail.fr>
2020-06-02 09:28:01 +0200

    clean folder

---
+++ AgenceProject/Assets/Scripts/Ulysse/RigManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D.Animation;
+
+public class RigManager : MonoBehaviour
+{
+    public GameObject[] Rigs;
+    int rigsCount;
+
+    void Start()
+    {
+        rigsCount = Rigs.Length;
+    }
+    void Update()
+    {
+        SwitchActive((int)PlayerController.playerState);
+    }
+
+    void SwitchActive(int activeRig)
+    {
+        for (int i = 0; i < rigsCount; i++)
+        {
+            if (i != activeRig)
+            {
+                Rigs[i].SetActive(false);
+            }
+            else
+            {
+                Rigs[i].SetActive(true);
+            }
+        }
+    }
+}
