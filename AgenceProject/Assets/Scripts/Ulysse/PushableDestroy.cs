f115bd6ff728d4bfcdad450cae87bf95ff691b2e
Flo <dappe.florent@gmail.com>
2020-06-04 10:20:22 +0200

    PushableWall Fix Destruction Anim

--- AgenceProject/Assets/Scripts/Ulysse/PushableDestroy.cs
+++ AgenceProject/Assets/Scripts/Ulysse/PushableDestroy.cs
@@ -3,6 +3,30 @@
 public class PushableDestruction : MonoBehaviour
 {
     public bool isDestroying;
+    private Material mat;
+    private float fade;
+
+    void Awake()
+    {
+        mat = GetComponent<SpriteRenderer>().material;
+        fade = BrickData.timerPushableDestroy;
+    }
+
+    void FixedUpdate()
+    {
+        if (isDestroying)
+        {
+            fade -= Time.deltaTime;
+
+            if (fade <= 0)
+            {
+                fade = 0;
+                isDestroying = false;
+            }
+
+            mat.SetFloat("_Fade", fade / BrickData.timerPushableDestroy);
+        }
+    }
 
     void OnCollisionEnter2D(Collision2D other)
     {
--- AgenceProject/Assets/Scripts/Ulysse/PushableWall.cs
+++ AgenceProject/Assets/Scripts/Ulysse/PushableWall.cs
@@ -8,36 +8,18 @@ public class PushableWall : MonoBehaviour
     private Vector2 otherVelocity;
     private GameObject parent;
     private Rigidbody2D parentRb;
-    private Material material;
-
-    bool isDissolving;
-    float fade = 2f;
 
     void Awake()
     {
         parent = transform.parent.gameObject;
         parent.AddComponent<PushableDestruction>();
         parentRb = parent.GetComponent<Rigidbody2D>();
-        material = parent.GetComponent<SpriteRenderer>().material;
     }
 
     void FixedUpdate()
     {
         transform.localPosition = Vector3.zero;
         transform.rotation = parent.transform.rotation;
-
-        if (isDissolving)
-        {
-            fade -= Time.deltaTime;
-
-            if (fade <= 0)
-            {
-                fade = 0;
-                isDissolving = false;
-            }
-
-            material.SetFloat("_Fade", fade);
-        }
     }
 
     void OnCollisionEnter2D(Collision2D other)
@@ -47,7 +29,6 @@ public class PushableWall : MonoBehaviour
             otherVelocity = other.GetContact(0).rigidbody.velocity;
             collidepos = other.GetContact(0).point;
             parentRb.AddForceAtPosition(-otherVelocity, collidepos,ForceMode2D.Impulse);
-            isDissolving = true;
         }
     }
 }
