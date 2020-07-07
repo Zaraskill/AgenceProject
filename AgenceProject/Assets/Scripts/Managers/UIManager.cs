ef4327ad3fca57736b3a65a32155d8abac9f3255
Flo <dappe.florent@gmail.com>
2020-07-07 11:23:38 +0200

    Revert "Lights 9 - 16 & Audio Victory"
    This reverts commit ccae5951d01e15842eb099eb4b6895cfa4416e06.

--- AgenceProject/Assets/Scripts/Managers/UIManager.cs
+++ AgenceProject/Assets/Scripts/Managers/UIManager.cs
@@ -890,7 +890,6 @@ public class UIManager : MonoBehaviour
         int index = SceneManager.GetActiveScene().buildIndex;
         if (hasWin)
         {
-            AudioManager.instance.Play("SFX_UI_Victory", false);
             imageStarsResults.sprite = dataResults.victory;
             textResults.GetComponent<TextLocaliserUI>().UpdateText("_victory");
             LevelManager.levelManager.starsObtained = starsUnlocked;
